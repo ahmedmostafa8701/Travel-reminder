59bbb4f8a9c8d893df6a8062feefca6a61ae4018
Ahmed Mostafa <ahmedmostafa8701@gmail.com>
2022-10-04 14:23:05 +0200

    First commit

---
+++ app/src/main/java/com/example/travelreminder/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.travelreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
